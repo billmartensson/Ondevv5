10b1cd21fff30354ee49874a3293c50868eecf0a
Bill Mårtensson <bill@magictechnology.se>
2024-11-13 13:05:11 +0100

    Initial Commit

---
+++ Ondevv5/Ondevv5App.swift
@@ -0,0 +1,17 @@
+//
+//  Ondevv5App.swift
+//  Ondevv5
+//
+//  Created by BillU on 2024-11-13.
+//
+
+import SwiftUI
+
+@main
+struct Ondevv5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
